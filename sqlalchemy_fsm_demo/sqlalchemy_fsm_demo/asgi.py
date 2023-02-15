c33bf6bfd233efbe11c643235bd0b1a12edca924
Alberto Mucarsel <albertmukarsel1397@hotmail.com>
2023-02-15 10:10:31 -0500

    Django boilerplate

---
+++ sqlalchemy_fsm_demo/sqlalchemy_fsm_demo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sqlalchemy_fsm_demo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sqlalchemy_fsm_demo.settings')
+
+application = get_asgi_application()
