b96bd9c77a6db6579a5302d513577eaf86a7622f
Alberto Mucarsel <albertmukarsel1397@hotmail.com>
2023-02-22 14:37:24 -0500

    translating functions

--- sqlalchemy_fsm_demo/proformas/views.py
+++ sqlalchemy_fsm_demo/proformas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from proformas.models import Base, Proforma
+from proformas.models import Base, PurchaseOrder
 from sqlalchemy_fsm_demo.settings import sa_engine, session
 from datetime import datetime
 import traceback
@@ -9,17 +9,17 @@ Base.metadata.create_all(sa_engine)
 
 # Validating DB is empty or not
 def is_empty():
-    return session.query(Proforma).count() <= 0
+    return session.query(PurchaseOrder).count() <= 0
 
 
 # Adding initial records
 def populate():
     today = datetime.now()
-    proformas_inciales = [
-        Proforma("Alberto Mucarsel", today, 150.45),
-        Proforma("Juan Perez", today, 2500.20),
+    initial_orders = [
+        PurchaseOrder("Alberto Mucarsel", today, 150.45),
+        PurchaseOrder("Juan Perez", today, 2500.20),
     ]
-    session.add_all(proformas_inciales)
+    session.add_all(initial_orders)
     session.commit()
 
 
@@ -27,8 +27,8 @@ def populate():
 def index(request):
     if is_empty():
         populate()
-    proformas = session.query(Proforma).order_by(Proforma.id).all()
-    return render(request, "proformas/index.html", {"proformas": proformas})
+    orders = session.query(PurchaseOrder).order_by(PurchaseOrder.id).all()
+    return render(request, "proformas/index.html", {"orders": orders})
 
 
 # Adding records to DB using session.add() from SA
@@ -36,7 +36,7 @@ def create(request):
     created = False
     if request.method == "POST":
         data = request.POST
-        session.add(Proforma(data["full_name"], data["date"], data["total_value"]))
+        session.add(PurchaseOrder(data["full_name"], data["date"], data["total_value"]))
         session.commit()
         created = True
     return render(request, "proformas/create.html", {"form_submitted": created})
@@ -45,35 +45,35 @@ def create(request):
 def update(request):
     if request.method == "POST":
         data = request.POST
-        current_file = session.query(Proforma).get(data["pk"])
-        result = "Proforma actualizada"
-        if data["state"] == "aprobada":
+        current_file = session.query(PurchaseOrder).get(data["pk"])
+        result = "Purchase order updated"
+        if data["state"] == "approved":
             # Calling transition-decorated methods
             try:
                 current_file.accept.set()
             except:
-                result = "Esta proforma ya fue aceptada"
-        elif data["state"] == "credito":
+                result = "This purchase order has been already accepted"
+        elif data["state"] == "credit":
             # .set() changes the state of the record object to the transitions' target state
             # (or raises an exception if it is not able to do so)
             try:
                 current_file.give_credit.set()
             # Handling Exceptions when transition cannot be done
             except Exception:
-                result = "No se puede otorgar crédito"
+                result = "Cannot grant credit to this purchase order"
                 traceback.print_exc()
-        elif data["state"] == "pagada":
+        elif data["state"] == "paid":
             try:
                 current_file.pago.set()
             except:
-                result = "No se puede cancelar hasta aceptarse primero"
-    return render(request, "proformas/result.html", {"resultado": result})
+                result = "Purchase order cannot be paid until accepted first"
+    return render(request, "proformas/result.html", {"result": result})
 
 
 def delete(request):
     if request.method == "POST":
         data = request.POST
-        current_file = session.query(Proforma).get(data["pk"])
+        current_file = session.query(PurchaseOrder).get(data["pk"])
         session.delete(current_file)
         session.commit()
-    return render(request, "proformas/result.html", {"resultado": "Proforma eliminada"})
+    return render(request, "proformas/result.html", {"result": "Purchase order marked as void"})
