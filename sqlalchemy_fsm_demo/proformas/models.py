ff2d6e4f95cc3c6b2134d430f0a3b6ee15476c8f
AlbertMukarsel <55022826+AlbertMukarsel@users.noreply.github.com>
2023-02-22 14:40:07 -0500

    Docstrings (#4)
    * Adding notes
    
    * translating functions

--- sqlalchemy_fsm_demo/proformas/models.py
+++ sqlalchemy_fsm_demo/proformas/models.py
@@ -9,51 +9,51 @@ Base = declarative_base()
 
 # Class object with two methods, no need to call specifically its methods
 # it will execute them based on the source state
-@transition(target="pagada")
-class ProformaHandler(object):
-    @transition(source="aceptada")
-    def pagar_contado(self, instance):
-        instance.side_effect = "Proforma cancelada de contado"
+@transition(target="paid")
+class PurchaseOrderHandler(object):
+    @transition(source="accepted")
+    def pay_cash(self, instance):
+        instance.side_effect = "Purchase order paid in cash"
 
-    @transition(source="credito")
-    def dar_credito(self, instance):
-        instance.side_effect = "Proforma otorgada crédito fue cancelada"
+    @transition(source="credit")
+    def give_credit(self, instance):
+        instance.side_effect = "Granted credit has been paid"
 
 
 # Generating a SA class, differs from a Django model
-class Proforma(Base):
-    __tablename__ = "proformas"
+class PurchaseOrder(Base):
+    __tablename__ = "purchase_orders"
     id = Column(Integer, primary_key=True)
     full_name = Column(String)
     creation_date = Column(Date)
     total_value = Column(Double)
     state = Column(FSMField, nullable=False)
-    pago = ProformaHandler
+    pago = PurchaseOrderHandler
 
-    def __init__(self, full_name, date, total_value, state="creada") -> None:
+    def __init__(self, full_name, date, total_value, state="created") -> None:
         self.full_name = full_name
         self.creation_date = date
         self.total_value = total_value
         self.state = state
 
     def __repr__(self) -> str:
-        return f"Proforma({self.full_name},{self.creation_date},{self.total_value},{self.state})"
+        return f"PurchaseOrder({self.full_name},{self.creation_date},{self.total_value},{self.state})"
 
-    # Verifying if the proforma value is greater than or equal to 200
+    # Verifying if the purchase order value is greater than or equal to 200
     # in order to grant credit
     def check_value(self) -> bool:
         if self.total_value >= 200:
             return True
         return False
 
-    @transition(source="creada", target="aceptada")
+    @transition(source="created", target="accepted")
     def accept(self):
-        print("Proforma aceptada")
+        print("Purchase order accepted")
 
     # Using a condition to verify it the state transition can be done
-    @transition(source="aceptada", target="credito", conditions=[check_value])
+    @transition(source="accepted", target="credit", conditions=[check_value])
     def give_credit(self):
-        print("Proforma procesada a credito")
+        print("Purchase order processed with credit")
 
     class Meta:
         app_label = "proformas"
